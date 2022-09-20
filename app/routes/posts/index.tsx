fdfee527d1dc12189a6173c17815ccbe0bd88585
Alejo Boga <alejoboga20@gmail.com>
2022-09-20 12:19:22 -0400

    Load Data

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,9 +1,47 @@
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
+
 const Posts = () => {
+  const { posts } = useLoaderData() as LoaderData;
+
   return (
     <main>
       <h1>Posts Page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
 
 export default Posts;
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Post[];
+};
